80c6185081bfcb8836e3520ab8224160b985ebce
sxf-1013 <2716030624@qq.com>
2021-04-25 20:35:18 +0800

    java.practice

--- src/JAVA/Cycle.java
+++ src/JAVA/Cycle.java
@@ -1,29 +1,46 @@
 package JAVA;
 
 public class Cycle {
-    public void play(){
+    public void play() {
         System.err.println("Cycle");
     }
+
+    public void wheels() {
+        System.out.println("轮子的数量是" + "num");
+
+    }
 }
+
 
 class Unicycle extends Cycle {
         public void play(){
             System.err.println("Unicycle");
         }
+        public void wheels(){
+            System.out.println("Unicycle wheels:4");
+        }
     }
 class Bicycle extends Cycle {
-    public void play(){
+    public void play() {
         System.err.println("Bicycle");
     }
-}
+        public void wheels(){
+            System.out.println("Bicycle wheels:2");
+        }
+    }
+
+
 class Tricycle extends Cycle {
     public void play(){
         System.err.println("Tricycle");
     }
+    public void wheels(){
+        System.out.println("Bicycle wheels:3S");
+    }
 }
-class Ride{
+class Ride {
 
-    public static void ride(Cycle cycle){
+    public static void ride(Cycle cycle) {
         cycle.play();
     }
 
@@ -34,7 +51,11 @@ class Ride{
         ride(unicycle);
         ride(bicycle);
         ride(tricycle);
+        unicycle.wheels();
+        bicycle.wheels();
+        tricycle.wheels();
 
     }
 }
+
 
