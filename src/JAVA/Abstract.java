2644a5bfd1eca73db918af3da94ce3d05340d386
sxf-1013 <2716030624@qq.com>
2021-04-30 19:56:19 +0800

    java.practice

---
+++ src/JAVA/Abstract.java
@@ -0,0 +1,11 @@
+package JAVA;
+
+public abstract class Abstract {
+}
+    class Abstract1 {
+        public static void main(String[] args) {
+            new Abstract();//error 如下图
+        }
+    }
+
+
