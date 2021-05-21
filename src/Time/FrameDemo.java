fdc2ea135ee7e15c3896982a3793cae26fd26fbc
sxf-1013 <2716030624@qq.com>
2021-05-21 20:08:30 +0800

    java.practice

---
+++ src/Time/FrameDemo.java
@@ -0,0 +1,12 @@
+package Time;
+
+import javax.swing.*;
+
+public class FrameDemo {
+    public static void main(String[] args){
+        MyFrameDate frame2=new MyFrameDate("第二个窗口");
+        frame2.showTime();
+
+    }
+
+}
