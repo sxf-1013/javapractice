fff42dfaa8364d5d27743ac981820f369a3c0b3f
sxf-1013 <2716030624@qq.com>
2021-05-26 22:50:35 +0800

    java.practice

--- src/Time/FrameDemo.java
+++ src/Time/FrameDemo.java
@@ -1,11 +1,15 @@
 package Time;
 
+import Time.sin.MyFrameSin;
+
 import javax.swing.*;
 
 public class FrameDemo {
     public static void main(String[] args){
-        MyFrameDate frame2=new MyFrameDate("第二个窗口");
-        frame2.showTime();
+       //MyFrameDate frame2=new MyFrameDate("第二个窗口");
+       //frame2.showTime();
+
+        MyFrameSin frameSin=new MyFrameSin("正弦曲线");
 
     }
 
